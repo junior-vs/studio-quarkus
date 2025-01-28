baa54339c363d896cb105da4c75cd5ec0a5d8ad3
VALDIR JUNIOR <valdir.junior.dev@gmail.com>
2025-01-28 09:11:24 -0300

    Add initial REST service implementation with Docker support and configuration files

---
+++ quarkus-integration/rest-service/src/main/java/br/studio/quarkus/integration/basic/Fruit.java
@@ -0,0 +1,38 @@
+package br.studio.quarkus.integration.basic;
+public class Fruit {
+
+    private String name;
+    private String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit [name=" + name + ", description=" + description + "]";
+    }
+
+    
+    
+}
