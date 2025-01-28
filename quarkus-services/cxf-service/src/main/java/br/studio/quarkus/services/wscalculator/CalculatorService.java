02f026d55a4d9c1ac795828eb5b181e2832cdfd5
Valdir Junior <valdir-souza.junior@caixa.gov.br>
2025-01-28 16:24:34 -0300

    exemplo de cliente soap

--- quarkus-services/cxf-service/src/main/java/br/studio/quarkus/services/wscalculator/CalculatorService.java
+++ quarkus-services/cxf-service/src/main/java/br/studio/quarkus/services/wscalculator/CalculatorService.java
@@ -21,33 +21,15 @@ public interface CalculatorService {
     @WebMethod
     public Integer divide(Integer a, Integer b);
 
-
-    /**
-     * @param operands a non-primitive parameter
-     * @return
-     */
     @WebMethod
     public Result addOperands(Operands operands);
 
-    /**
-     * @param a        the first operand
-     * @param operands the second two operands
-     * @return sum of a, {@link Operands#getA()} and {@link Operands#getB()}
-     */
     @WebMethod
     public int addNumberAndOperands(int a, Operands operands);
 
-    /**
-     * @param array an array of numbers to sum
-     * @return sum of the given {@code array} elements
-     */
     @WebMethod
     public int addArray(int... array);
 
-    /**
-     * @param list a list of numbers to sum
-     * @return sum of the given {@code list} elements
-     */
     @WebMethod
     public int addList(List<Integer> list);
 
